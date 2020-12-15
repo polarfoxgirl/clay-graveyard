7f43cde0a7a79a95c4031949773bb92b6127dd12
Olga Nikonova <helga.nikonova@gmail.com>
2020-12-14 20:31:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+tiles.setTilemap(tilemap`level`)
+let my_sprite = sprites.create(img`
+    . . . . . . . . . . . . . .
+    e e e . . . . e e e . . . .
+    c d d c . . c d d c . . . .
+    c b d d f f d d b c . . . .
+    c 3 b d d b d b 3 c . . . .
+    f b 3 d d d d 3 b f . . . .
+    e d d d d d d d d e . . . .
+    e d f d d d d f d e . b f b
+    f d d f d d f d d f . f d f
+    f b d d b b d d 2 b f f d f
+    . f 2 2 2 2 2 2 d b b d b f
+    . f d d d d d d d f f f f .
+    . . f d b d f d f . . . . .
+    . . . f f f f f f . . . . .
+`)
+controller.moveSprite(my_sprite)
---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100006060606060606060606060606060606060102020303010101040402010204060604030302030204040204040404040606040404040302050202020204020206060302010204010401040301040202060602030104020102020305010202020606040402040101030303030302030306060102040202010202020104020302060601020401010103020104020101020606010103010403030301020302010406060302010101010202030202030104060603030202030103020302010102010606030402020302030303030302020406060402030302020203010302020202060603020203010303020102040402020606060606060606060606060606060606`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.builtin.forestTiles0,sprites.builtin.forestTiles4], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
